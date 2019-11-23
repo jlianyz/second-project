aac7c4efb6995ef46290133dae9eb23cb1542a50
Ubuntu <ubuntu@ip-172-31-91-196.ec2.internal>
2019-11-23 10:05:53 +0000

    added api to pull pokemon data

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,24 @@ var next = document.getElementById("#next");
 var counter = 0;
 var learnMore = document.getElementById("#showPokemon")
 var difficultyLevel;
+
+//get api data for image and hint
+function getPokemonData() {
+  //var input = Math.ceil(Math.random() * 151 + 1);
+  axios.get(apiUrl + Math.ceil(Math.random() * 151 + 1))
+  .then(function (response) {
+    pokemonName.innerHTML = response.data.forms[0].name;
+    pokemonImage.src = response.data.sprites.front_default;
+    pokemonType = response.data.types[0].type.name;
+    wordLength = response.data.forms[0].name.length;
+    getHint.innerHTML = "Hint: This pokemon is " + pokemonType + " type and has " + wordLength + " letters.";
+    $(".pokemon-image").show();
+    $(".pokemon-name").show();
+    $(".pokemon-name").css("display", "none");
+    $("#hint").show();           
+  })
+  .catch(function (error) {
+    pokemonName.innerHTML = "(An error has occurred.)";
+    pokemonImage.src = "";
+  });
+}  
